6a1be56ca62e34f77190a5a560a2a4c0c7dfc891
gautk1ng <gautk1ng@gmail.com>
2025-04-21 19:23:30 +0200

    Finis le ep3

--- s2/ep3/generics.ts
+++ s2/ep3/generics.ts
@@ -5,9 +5,14 @@ interface Voiture {
 }
 
 function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+    console.log(obj, key)
     return obj[key]
 }
 
-let maVoiture: Voiture
+let maVoiture: Voiture = {
+    marque: "Ferrari",
+    modele: "F40",
+    annee: 1987
+}
 
-getProperty({maVoiture.marque, maVoiture.annee})
+getProperty(maVoiture.marque, maVoiture.annee)
